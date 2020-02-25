5bfe1e633dec9c4e86b73eaab6cd69f0906f062b
Mirko <mirko.longo.1987@gmail.com>
2020-02-25 14:17:49 +0100

    Creazione scopo e struttura html linkata

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// SCOPO DEL GIOCO:
+// 1. Il computer deve generare 16 numeri casuali tra 1 e 100.
+// 2. In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// 3. Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// 4. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// 5. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// 6. BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
+//           Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50
